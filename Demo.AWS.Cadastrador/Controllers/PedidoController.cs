3cdd96e70d9d946517e8465319b8892dde92077d
Robson de Moraes Silva <rms2307@outlook.com>
2022-06-26 17:06:14 -0300

    Criando primeira API (Cadastrador) para ser publicado no AWS Lambda

---
+++ Demo.AWS.Cadastrador/Controllers/PedidoController.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using Common.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.AWS.Cadastrador.Controllers
+{
+    [Route("api/[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        [HttpPost]
+        public async Task PostAsync([FromBody] Pedido pedido)
+        {
+            pedido.Id = Guid.NewGuid().ToString();
+            pedido.DataDeCriacao = DateTime.UtcNow;
+
+            await pedido.SalvarAsync();
+
+            Console.WriteLine($"Pedido salvo com sucesso: id {pedido.Id}");
+        }
+    }
+}
